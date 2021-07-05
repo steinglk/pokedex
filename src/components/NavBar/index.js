1b3fe1c9f334eca79c06613ae2a34684c517caea
Matheus Stein <soloqstein@outlook.com>
2021-07-05 16:40:19 -0300

    Navbar implementada

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import useStyles from './style'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+export default function PersistentDrawerRight() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.toolbar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap className={classes.title}>
+            Pokédex
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
